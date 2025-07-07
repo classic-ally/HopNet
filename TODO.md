c8e29b43aeaf5555fef23797186cb4e724a695fd
Allison Bentley <allison@bentley.sh>
2025-07-07 16:46:48 +0100

    add todo and nix-shell to repo

---
+++ TODO.md
@@ -0,0 +1,2 @@
+[] - Switch any IDs in database to U versions (UINTEGER etc) and the corresponding rust side to u32 etc
+[] - Zero-byte file handling in post_files: currently they meet criteria for chunking but should not be created
---
+++ shell.nix
@@ -0,0 +1,24 @@
+# Import the Nixpkgs library
+with import <nixpkgs> {};
+
+# Define the Nix expression for the app + its environment
+stdenv.mkDerivation {
+  name = "rust";
+
+  # Specify the packages to be available in the environment
+  buildInputs = [
+    pkgs.cargo
+    pkgs.rustc
+    pkgs.rust-analyzer
+
+    # openssl
+    pkgs.openssl.dev
+    pkgs.pkg-config
+
+    # frontend
+    pkgs.nodejs_24
+  ];
+  
+  shellHook = ''
+  '';
+}
